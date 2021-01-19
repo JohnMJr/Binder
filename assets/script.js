aea85ee42b1bad00959ebf0aac01e0a1a37c3806
jake-tho <73851971+jake-tho@users.noreply.github.com>
2021-01-19 01:00:44 -0500

    duplicated main page and added links to nav

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,28 @@
 
 //Global Variables
-let dictLookup = $('#dictLookup');
-let courseArray = ['math', 'science', 'english', 'history' ]
-let save = $('#saveBtn')
-let googleCal = $('#googleCal')
-const dictKey = 'f9a039f2-30bb-4050-b54e-4b87b2fa7d8e'
-let searchInput = $('#dictInput').value
-
+const dictionaryKey = 'f9a039f2-30bb-4050-b54e-4b87b2fa7d8e';
+let subjectArray = ['math', 'science', 'english', 'history'];
+let save = $('#saveBtn');
+let googleCal = $('#googleCal');
 
 
 // Creates event (click) listener to fire when the div class .searchBtn is pressed for searching and opening a new tab with the miriam webster page of the word that was searched
-$('#searchBtn ').on('click', function(){
-    pushInput()
-// actual api call https://www.dictionaryapi.com/api/v3/references/collegiate/json/ + searchInput + ?key= + dictKey
-});
+$('#searchBtn').on('click', function(){
+    let searchInput = $('#dictionaryInput').val();
+    $.ajax({url: "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + searchInput + "?key=" + dictionaryKey})
+    });
+    
+// actual api call https://www.dictionaryapi.com/api/v3/references/collegiate/json/ + searchInput + ?key= + dictionaryKey
+
+
 
 
 // Event listener for when the Save Note button is pressed to save the updated note to local storage
 
-$('#saveBtn ').on('click', function(){
-    
-
+$('#saveBtn').on('click', function saveNote (obj){
+    saveNote.obj = obj;
+    // saveNote.foo = foo
+    saveNote.val
 });
 
 // Event listener for switching between the subjects and hiding the active note and unhiding the inactive note
