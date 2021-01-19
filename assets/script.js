6e2357b8396c1f16cfe9fa5bb0a5ae8bc2a07e6d
John Meadows Jr <johnmjr25@gmail.com>
2021-01-19 11:38:07 -0500

    updating files

--- assets/script.js
+++ assets/script.js
@@ -4,38 +4,52 @@ const dictionaryKey = 'f9a039f2-30bb-4050-b54e-4b87b2fa7d8e';
 let subjectArray = ['math', 'science', 'english', 'history'];
 let save = $('#saveBtn');
 let googleCal = $('#googleCal');
+let noteData = $('#Note');
 
 
 // Creates event (click) listener to fire when the div class .searchBtn is pressed for searching and opening a new tab with the miriam webster page of the word that was searched
 $('#searchBtn').on('click', function(){
     let searchInput = $('#dictionaryInput').val();
-    $.ajax({url: "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + searchInput + "?key=" + dictionaryKey, success: function(result){
-    $("#dictionary").textContent(result);
-  }});
+    $.ajax({
+        url: "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + searchInput + "?key=" + dictionaryKey,
+        method: "GET"
+    }).then(function(response){
+        $("#dictionary").text(JSON.stringify(response));
+    }); 
 });
 
+
     
-// actual api call https://www.dictionaryapi.com/api/v3/references/collegiate/json/ + searchInput + ?key= + dictionaryKey
 
+function saveNote(){
+    localStorage.setItem('Note', JSON.stringify(noteData));
+}
 
+function displayNote(){
+    return noteData.text || 'Note';
+}
+
+function loadSave(){
+    let storedData = JSON.parse(localStorage.getItem('Note'));
+
+    if (storedData) {
+        noteData = storedData;
+    }
+
+    saveNote();
+    displayNote();
+}
+
+loadSave();
 
 
 // Event listener for when the Save Note button is pressed to save the updated note to local storage
-
-// Store your data.
-function saveStuff(obj) {
-    saveData.obj = obj;
-    // saveData.foo = foo;
-    saveData.time = new Date().getTime();
-    localStorage.saveData = JSON.stringify(saveData);
-  }
-
-// Event listener for switching between the subjects and hiding the active note and unhiding the inactive note
-
-
-// Event listener for when the add to calander button is pressed it brings up a multi input prompt for adding an event to google cal based off of event, date, and time
-
-
-
-
-
+$('#saveBtn').on('click', function (event) {
+    event.preventDefault();
+    let saveIndex = $(this).siblings('.form;').children('.Note').attr('id');
+    noteData[saveIndex].reminder = $(this.siblinings(".form").children(".Note").val();
+    console.log(saveIndex);
+    saveNote();
+    displayNote();
+})
+   
